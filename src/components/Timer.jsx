9e6b4dcc7a8b6e3bc25d6e87adde34b376da5346
marcos <marcosrossoni9@gmail.com>
2024-01-15 13:56:22 -0300

    conclusao

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import TimerDisplay from "./TimerDisplay.jsx";
 import TimerControls from "./TimerControls.jsx";
 import LapList from "./LapList.jsx";
 import "./Timer.css"
 
 const Timer = () => {
+
+    const [milliseconds, setMilliseconds] = useState(0)
+    const [timerOn, setTimerOn] = useState(false)
+    const [laps, setLaps] = useState([])
+
+
+    const formatTime = () => {
+        const minutes = ("0" + Math.floor(milliseconds / 60000) % 60).slice(-2)
+        const seconds = ("0" + Math.floor(milliseconds / 1000) % 60).slice(-2)
+        const centseconds = ("0" + ((milliseconds / 10) % 100)).slice(-2)
+
+        return `${minutes}:${seconds}:${centseconds}`
+    }
+
+    const startTimer = (interval) => {
+        return setInterval(() => {
+            setMilliseconds(prevMilliseconds => prevMilliseconds + 10)
+        }, 10)
+    }
+
+    const stopTimer = (interval) => {
+
+        clearInterval(interval)
+        return interval
+    }
+
+    const resetTimer = () => {
+        setMilliseconds(0)
+        setTimerOn(false)
+        setLaps([])
+    }
+
+    const addLap = () => {
+        setLaps([...laps, formatTime()])
+    }
+
+    useEffect(() => {
+        let interval = null
+        if (timerOn) {
+            interval = startTimer(interval)
+        } else {
+            interval = stopTimer(interval)
+        }
+
+        return () => stopTimer(interval)
+    }, [timerOn]);
+
     return (
         <div className="timer-container">
-            <TimerDisplay/>
-            <TimerControls/>
-            <LapList/>
+            <TimerDisplay time={formatTime()}/>
+            <TimerControls
+                timerOn={timerOn}
+                onStart={() => setTimerOn(true)}
+                onStop={() => setTimerOn(false)}
+                onReset={resetTimer}
+                onLap={addLap}/>
+            <LapList laps={laps}/>
         </div>
     );
 };
--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const TimerControls = () => {
+const TimerControls = ({onStart, onStop, timerOn, onReset, onLap}) => {
     return (
-        <div>
-            <button>Iniciar</button>
-            <button>Zerar</button>
+        <div className="timer-controls">
+            {!timerOn && <button onClick={onStart}>Iniciar</button>}
+            {timerOn && <button onClick={onStop}>Parar</button>}
+            {timerOn && <button onClick={onLap}>Volta</button>}
+            <button onClick={onReset}>Zerar</button>
         </div>
     );
 };
